ac30b48870764fe7f6d784ee1695c0a36f722ee5
Eliott CHEMINAL <eliott.cheminal@soitec.com>
2025-06-10 10:22:57 +0200

    keep faith

--- tests/picturetest.php
+++ tests/picturetest.php
@@ -24,7 +24,6 @@ class picturetest extends TestCase
         $controller->indexAction();
         $output = ob_get_clean();
         // Vérifie que le message d'erreur est présent
-        
         $this->assertStringContainsString('Une photo est obligatoire', $output);
     }
 }
