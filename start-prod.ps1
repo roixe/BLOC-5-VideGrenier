b13aefd69b11794a719356ceedbf3d65022e5b6c
Eliott <eliottcheminal@gmail.com>
2025-07-01 11:14:59 +0200

    doc api

--- start-prod.ps1
+++ start-prod.ps1
@@ -4,4 +4,4 @@ git checkout main
 
 git pull origin main
 
-docker-compose -p videgrenier-bloc5-prod --env-file .env.dev -f docker-compose.yml up --build -d
+docker-compose -p videgrenier-bloc5-prod --env-file .env.prod -f docker-compose.yml up --build -d
